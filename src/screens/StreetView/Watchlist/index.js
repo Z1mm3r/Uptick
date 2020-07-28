31c7666726ef1d2d7f2f59c295ba6af188e92c29
Z1mm3r <czimmer@ramapo.edu>
2020-07-28 19:00:23 -0400

    skeleton

---
+++ src/screens/StreetView/Watchlist/index.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+
+
+import TickerCard from '../../../components/TickerCard'
+import { Typography } from '@material-ui/core';
+
+const testValues = [
+    {symbol: 'AMD', price: '$78.54', change: '9.5' },
+    {symbol: 'WDC', price: '$4.55', change: '-4.4' },
+    {symbol: 'BRK.B', price: '$3095.30', change: '0.3' },
+    {symbol: 'SPCE', price: '$70.40', change: '1.1' },
+]
+
+function Watchlist(props){
+
+    const renderTickerCard = (values) =>{
+        return(
+            <Grid key = {values.symbol} item>
+                <TickerCard symbol={values ? values.symbol : null} 
+                price={values? values.price : null} 
+                change ={values ? values.change : null} 
+                borderThickness = 'medium'>
+                </TickerCard>
+            </Grid>
+        )
+    }
+
+    const renderTickerCards = (values) => {
+        return values.map(value => {
+            return renderTickerCard(value)
+        })
+    }
+
+    return(
+        <Grid container direction='column'>
+            <Grid item>
+                <Typography>
+                    <Box fontWeight="fontWeightBold" fontSize={30}>
+                        Watchlist
+                    </Box>
+                </Typography>
+            </Grid>
+            {renderTickerCards(testValues)}
+        </Grid>
+    )
+}
+
+export default Watchlist
